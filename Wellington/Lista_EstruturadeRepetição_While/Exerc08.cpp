9aba984c66b0bf53307742a3dbb3b47a158227d6
renitro <renan.nogueira01@fatec.sp.gov.br>
2017-07-18 15:45:20 -0300

    Update Exerc08.cpp

--- Wellington/Lista_EstruturadeRepetição_While/Exerc08.cpp
+++ Wellington/Lista_EstruturadeRepetição_While/Exerc08.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <locale.h>
-// 7 - Solicitar a idade de várias pessoas e imprimir: Total de pessoas com menos de 21 anos. Total de pessoas com mais de 50 anos. O programa termina quando idade for = -99.
-
+/* 8 - Solicitar um número entre 1 e 4. Se a pessoas digitar um número diferente,
+mostrar a mensagem "entrada inválida" e solicitar o número novamente. Se
+digitar correto mostrar o número digitado..
+*/
 main()
 {
 	setlocale(LC_ALL, "Portuguese");
@@ -9,21 +11,21 @@ main()
   int num;
   do
   {
-    printf("Digite um número entre 1 e 4: );
+    printf("Digite um número entre 1 e 4: ");
     scanf("%i", &num);
    
     switch(num)
     {
-        case 1: printf("\n\nNúmero digitado: 1);
+        case 1: printf("\n\nNúmero digitado: 1");
         break;
-        case 2: printf("\n\nNúmero digitado: 2);
+        case 2: printf("\n\nNúmero digitado: 2");
         break;
-        case 3: printf("\n\nNúmero digitado: 3);
+        case 3: printf("\n\nNúmero digitado: 3");
         break;
-        case 4: printf("\n\nNúmero digitado: 4);
+        case 4: printf("\n\nNúmero digitado: 4");
         break;
       
-        default: printf("\n\nEntrada inválida!\n);
+        default: printf("\n\nEntrada inválida!\n\n");
         break;
     }     
   
